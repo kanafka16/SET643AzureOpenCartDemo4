281d48064a8a61f4c27cf07592a8495dd10af1dd
kanafka16 <yurii.kanafotskyi@gmail.com>
2022-02-03 19:56:16 +0200

    headless add

--- OpenCartTests/Tools/TestRunner.cs
+++ OpenCartTests/Tools/TestRunner.cs
@@ -18,8 +18,14 @@ namespace OpenCartTests.Tools
         [SetUp]
         public void BeforeEachMethod()
         {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("headless");
+            options.AddArgument("disable-gpu");
+            options.AddArgument("no-sandbox");
+            options.AddArgument("proxy-server='direct://'");
+            options.AddArgument("proxy-bypass-list=*");
 
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl(OpenCartURL);
